ce3f1cc28906433df0bd03a74d44d164121ab6c5
Bipul Chaudhary <bipulchau@gmail.com>
2021-09-22 13:11:19 -0400

    In client.js, exported the connect function using ES6 Shorthand syntax. Imported it in play.js.

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.223.133',
+    port: 50542,
+  });
+  const name = 'Bipul';
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  //stdin.setEncoding('utf8');
+  conn.write(`${name} has connected!!!`);
+  conn.on('data', (data) => {
+    console.log("DATA CAME IN!!!!!");
+    console.log(data);
+})
+
+  return conn;
+};
+
+module.exports = {connect};
