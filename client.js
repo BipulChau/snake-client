77d7037b33862d5166af02668927b4e16822e1d1
Bipul Chaudhary <bipulchau@gmail.com>
2021-09-22 14:08:00 -0400

    added connection event handler and name command

--- client.js
+++ client.js
@@ -1,4 +1,6 @@
 const net = require("net");
+const stdin = process.stdin;
+
 
 // establishes a connection with the game server
 const connect = function () {
@@ -6,16 +8,30 @@ const connect = function () {
     host: '135.23.223.133',
     port: 50542,
   });
-  const name = 'Bipul';
+
+  const name = 'Bch';
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-  //stdin.setEncoding('utf8');
+  stdin.setEncoding('utf8');
+
   conn.write(`${name} has connected!!!`);
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server!")
+    //console.log(data);
+})
+
+stdin.on('data', (input) => {
+  conn.write(`Name: ${input}`)
+})
+
   conn.on('data', (data) => {
     console.log("DATA CAME IN!!!!!");
     console.log(data);
 })
+
+
 
   return conn;
 };
